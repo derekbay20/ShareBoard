9e244a1beb426a83e6f2dbec83d11ea533a2353b
derekbay20 <77827806+derekbay20@users.noreply.github.com>
2021-02-28 13:08:01 +0545

    Add files via upload

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php 
+ 
+//define DB params
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "");
+define("DB_NAME", "shareboard");
+
+define("ROOT_PATH", "/php.project/");
+define("ROOT_URL", "http://localhost/php.project/");
